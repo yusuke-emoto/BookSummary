7e0dd250e571a36a2ecd658a47a72fb805808665
yusuke-emoto <emoyuu0824@gmail.com>
2020-06-26 16:01:45 +0900

    ホームレイアウト実装、スキーマとマスターユーザの登録

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.demo.login.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 
 @Controller
@@ -9,8 +10,17 @@ public class HomeController {
 	//ホーム画面のGET用メソッド
 	@GetMapping("/home")
 	public String getHome(Model model) {
+		//コンテンツ部分にホーム画面を表示するために文字列をModelに登録
+		model.addAttribute("contents","home/home :: home_contents");
 		System.out.println(model);
 		return "home/homeLayout";
+	}
+	//ログアウト用メソッド
+	@PostMapping("/logout")
+	public String postLogout() {
+		//ログイン画面へリダイレクト
+		return "redirect:/login";
+		
 	}
 
 }
